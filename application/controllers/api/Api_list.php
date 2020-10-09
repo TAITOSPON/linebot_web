6318faab3edf4093af3a505a0e21e8619e2d7f67
TAITOSPON <totsapon_p@thaitobacco.or.th>
2020-10-09 14:34:51 +0700

    commit nueng add login view

--- application/controllers/api/Api_list.php
+++ application/controllers/api/Api_list.php
@@ -2,14 +2,16 @@
 defined('BASEPATH') OR exit('No direct script access allowed');
 require(APPPATH.'libraries/REST_Controller.php');
 
-class Api_list extends REST_Controller
-{
+class Api_list extends REST_Controller{
+
        public function index_get(){}
 
 
-       public function list_user_get(){
+       public function list_log_login_get(){
 
-              $main = $this->db->select('*')->get('lb_login_log')->result_array();
+              $main = $this->db->select('*')
+                            ->get('lb_login_log')
+                            ->result_array();
               echo json_encode($main,JSON_PRETTY_PRINT);
 
        }
